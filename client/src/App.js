faa6626468fa9e107a36c7e87300f409576805e5
MBrennecke <MBrennecke10@gmail.com>
2018-10-04 13:21:03 -0600

    added git ignore

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+const App = () => (
+	<Router>
+	  <Fragment>
+		<Nav />
+		<Switch>
+			<Route exact path="/" component={Books} />
+			<Route exact path="/books" component={Books} />
+			<Route exact path="/books/:id" component={Detail} />
+			<Route component={NoMatch} />
+		</Switch>
+	  </Fragment>
+  </Router>
+);
+
+export default App;
